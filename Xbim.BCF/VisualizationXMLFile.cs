a194f0595d0ab9f7e923a53f9bf4b23b9fdff8e4
briakovai <35344498+briakovai@users.noreply.github.com>
2021-12-20 17:17:54 +0600

    Доработан класс VisualizationXMLFile и его классы-наследники.

--- Xbim.BCF/VisualizationXMLFile.cs
+++ Xbim.BCF/VisualizationXMLFile.cs
@@ -7,56 +7,145 @@ using Xbim.BCF.XMLNodes;
 
 namespace Xbim.BCF
 {
-	public abstract class VisualizationXMLFile { }
+	internal class VisualizationXMLFileConstants
+	{
+		public const string VISUALIZATION_INFO = "VisualizationInfo";
+	}
 
 	[Serializable]
-	[XmlType("VisualizationInfo")]
+	public abstract class VisualizationXMLFile
+	{
+		protected internal BCFOrthogonalCamera _orthogonalCamera;
+
+		public void SetOrthogonalCamera(BCFOrthogonalCamera orthogonalCamera)
+		{
+			_orthogonalCamera = orthogonalCamera;
+		}
+
+		public BCFOrthogonalCamera GetOrthogonalCamera()
+		{
+			return _orthogonalCamera;
+		}
+
+		protected internal BCFPerspectiveCamera _perspectiveCamera;
+
+		public void SetPerspectiveCamera(BCFPerspectiveCamera orthogonalCamera)
+		{
+			_perspectiveCamera = orthogonalCamera;
+		}
+
+		public BCFPerspectiveCamera GetPerspectiveCamera()
+		{
+			return _perspectiveCamera;
+		}
+
+		protected internal List<BCFLine> _lines;
+
+		public void SetLines(List<BCFLine> lines)
+		{
+			_lines = lines;
+		}
+
+		public List<BCFLine> GetLines()
+		{
+			return _lines;
+		}
+
+		protected internal List<BCFClippingPlane> _clippingPlanes;
+
+		public void SetClippingPlanes(List<BCFClippingPlane> clippingPlanes)
+		{
+			_clippingPlanes = clippingPlanes;
+		}
+
+		public List<BCFClippingPlane> GetClippingPlanes()
+		{
+			return _clippingPlanes;
+		}
+
+		protected internal List<BCFBitmap> _bitmaps;
+
+		public void SetBitmaps(List<BCFBitmap> bitmaps)
+		{
+			_bitmaps = bitmaps;
+		}
+
+		public List<BCFBitmap> GetBitmaps()
+		{
+			return _bitmaps;
+		}
+
+		protected internal void SetFileds(XDocument xdoc)
+		{
+			_lines = new List<BCFLine>();
+			_clippingPlanes = new List<BCFClippingPlane>();
+			_bitmaps = new List<BCFBitmap>();
+
+			var orth = xdoc.Root.Elements("OrthogonalCamera").FirstOrDefault();
+			if (orth != null)
+			{
+				_orthogonalCamera = new BCFOrthogonalCamera(orth);
+			}
+
+			var pers = xdoc.Root.Elements("PerspectiveCamera").FirstOrDefault();
+			if (pers != null)
+			{
+				_perspectiveCamera = new BCFPerspectiveCamera(pers);
+			}
+
+			var lines = xdoc.Root.Elements("Lines").FirstOrDefault();
+			if (lines != null)
+			{
+				foreach (var line in xdoc.Root.Element("Lines").Elements("Line"))
+					_lines.Add(new BCFLine(line));
+			}
+
+			var clippingplanes = xdoc.Root.Elements("ClippingPlanes").FirstOrDefault();
+			if (clippingplanes != null)
+			{
+				foreach (var plane in xdoc.Root.Element("ClippingPlanes").Elements("ClippingPlane"))
+					_clippingPlanes.Add(new BCFClippingPlane(plane));
+			}
+
+			var bitmaps = xdoc.Root.Elements("Bitmaps").FirstOrDefault();
+			if (bitmaps != null)
+			{
+				foreach (var bmap in xdoc.Root.Elements("Bitmaps"))
+					_bitmaps.Add(new BCFBitmap(bmap));
+			}
+		}
+	}
+
+	[Serializable]
+	[XmlType(VisualizationXMLFileConstants.VISUALIZATION_INFO)]
 	public class VisualizationXMLFile_lower_2_1 : VisualizationXMLFile
 	{
 		[XmlArray(Order = 1)]
 		public List<BCFComponent> Components;
 
 		[XmlElement(Order = 2)]
-		public BCFOrthogonalCamera OrthogonalCamera { get; set; }
+		public BCFOrthogonalCamera OrthogonalCamera { get { return _orthogonalCamera; } set { _orthogonalCamera = value; } }
 
 		[XmlElement(Order = 3)]
-		public BCFPerspectiveCamera PerspectiveCamera { get; set; }
+		public BCFPerspectiveCamera PerspectiveCamera { get { return _perspectiveCamera; } set { _perspectiveCamera = value; } }
 
 		[XmlArray(Order = 4)]
-		public List<BCFLine> Lines;
+		public List<BCFLine> Lines { get { return _lines; } set { _lines = value; } }
 
 		[XmlArray(Order = 5)]
-		public List<BCFClippingPlane> ClippingPlanes;
+		public List<BCFClippingPlane> ClippingPlanes { get { return _clippingPlanes; } set { _clippingPlanes = value; } }
 
-		[XmlElement(ElementName = "Bitmaps", Order = 6)]
-		public List<BCFBitmap> Bitmaps;
+		[XmlElement(Order = 6)]
+		public List<BCFBitmap> Bitmaps { get { return _bitmaps; } set { _bitmaps = value; } }
 
 		public VisualizationXMLFile_lower_2_1()
 		{
 			Components = new List<BCFComponent>();
-			Lines = new List<BCFLine>();
-			ClippingPlanes = new List<BCFClippingPlane>();
-			Bitmaps = new List<BCFBitmap>();
 		}
 
 		public VisualizationXMLFile_lower_2_1(XDocument xdoc)
 		{
 			Components = new List<BCFComponent>();
-			Lines = new List<BCFLine>();
-			ClippingPlanes = new List<BCFClippingPlane>();
-			Bitmaps = new List<BCFBitmap>();
-
-			var orth = xdoc.Root.Elements("OrthogonalCamera").FirstOrDefault();
-			if (orth != null)
-			{
-				OrthogonalCamera = new BCFOrthogonalCamera(orth);
-			}
-
-			var pers = xdoc.Root.Elements("PerspectiveCamera").FirstOrDefault();
-			if (pers != null)
-			{
-				PerspectiveCamera = new BCFPerspectiveCamera(pers);
-			}
 
 			if (xdoc.Root.Element("Components") != null && xdoc.Root.Element("Components").Elements("Component") != null)
 			{
@@ -64,77 +153,40 @@ namespace Xbim.BCF
 					Components.Add(new BCFComponent(comp));
 			}
 
-			var lines = xdoc.Root.Elements("Lines").FirstOrDefault();
-			if (lines != null)
-			{
-				foreach (var line in xdoc.Root.Element("Lines").Elements("Line"))
-					Lines.Add(new BCFLine(line));
-			}
-
-			var clippingplanes = xdoc.Root.Elements("ClippingPlanes").FirstOrDefault();
-			if (clippingplanes != null)
-			{
-				foreach (var plane in xdoc.Root.Element("ClippingPlanes").Elements("ClippingPlane"))
-					ClippingPlanes.Add(new BCFClippingPlane(plane));
-			}
-
-			var bitmaps = xdoc.Root.Elements("Bitmaps").FirstOrDefault();
-			if (bitmaps != null)
-			{
-				foreach (var bmap in xdoc.Root.Elements("Bitmaps"))
-					Bitmaps.Add(new BCFBitmap(bmap));
-			}
+			SetFileds(xdoc);
 		}
 	}
 
 	[Serializable]
-	[XmlType("VisualizationInfo")]
+	[XmlType(VisualizationXMLFileConstants.VISUALIZATION_INFO)]
 	public class VisualizationXMLFile_2_1 : VisualizationXMLFile
 	{
 		[XmlElement(ElementName = "Components", Order = 1)]
 		public BCFComponents Components_2_1;
 
 		[XmlElement(Order = 2)]
-		public BCFOrthogonalCamera OrthogonalCamera { get; set; }
+		public BCFOrthogonalCamera OrthogonalCamera { get { return _orthogonalCamera; } set { _orthogonalCamera = value; } }
 
 		[XmlElement(Order = 3)]
-		public BCFPerspectiveCamera PerspectiveCamera { get; set; }
+		public BCFPerspectiveCamera PerspectiveCamera { get { return _perspectiveCamera; } set { _perspectiveCamera = value; } }
 
 		[XmlArray(Order = 4)]
-		public List<BCFLine> Lines;
+		public List<BCFLine> Lines { get { return _lines; } set { _lines = value; } }
 
 		[XmlArray(Order = 5)]
-		public List<BCFClippingPlane> ClippingPlanes;
+		public List<BCFClippingPlane> ClippingPlanes { get { return _clippingPlanes; } set { _clippingPlanes = value; } }
 
-		[XmlElement(ElementName = "Bitmaps", Order = 6)]
-		public List<BCFBitmap> Bitmaps;
+		[XmlElement(Order = 6)]
+		public List<BCFBitmap> Bitmaps { get { return _bitmaps; } set { _bitmaps = value; } }
 
 		public VisualizationXMLFile_2_1()
 		{
 			Components_2_1 = new BCFComponents();
-			Lines = new List<BCFLine>();
-			ClippingPlanes = new List<BCFClippingPlane>();
-			Bitmaps = new List<BCFBitmap>();
 		}
 
 		public VisualizationXMLFile_2_1(XDocument xdoc)
 		{
 			Components_2_1 = new BCFComponents();
-			Lines = new List<BCFLine>();
-			ClippingPlanes = new List<BCFClippingPlane>();
-			Bitmaps = new List<BCFBitmap>();
-
-			var orth = xdoc.Root.Elements("OrthogonalCamera").FirstOrDefault();
-			if (orth != null)
-			{
-				OrthogonalCamera = new BCFOrthogonalCamera(orth);
-			}
-
-			var pers = xdoc.Root.Elements("PerspectiveCamera").FirstOrDefault();
-			if (pers != null)
-			{
-				PerspectiveCamera = new BCFPerspectiveCamera(pers);
-			}
 
 			if (xdoc.Root.Element("Components") != null && xdoc.Root.Element("Components").Elements("Component") != null)
 			{
@@ -142,26 +194,7 @@ namespace Xbim.BCF
 				Components_2_1 = new BCFComponents(components);
 			}
 
-			var lines = xdoc.Root.Elements("Lines").FirstOrDefault();
-			if (lines != null)
-			{
-				foreach (var line in xdoc.Root.Element("Lines").Elements("Line"))
-					Lines.Add(new BCFLine(line));
-			}
-
-			var clippingplanes = xdoc.Root.Elements("ClippingPlanes").FirstOrDefault();
-			if (clippingplanes != null)
-			{
-				foreach (var plane in xdoc.Root.Element("ClippingPlanes").Elements("ClippingPlane"))
-					ClippingPlanes.Add(new BCFClippingPlane(plane));
-			}
-
-			var bitmaps = xdoc.Root.Elements("Bitmaps").FirstOrDefault();
-			if (bitmaps != null)
-			{
-				foreach (var bmap in xdoc.Root.Elements("Bitmaps"))
-					Bitmaps.Add(new BCFBitmap(bmap));
-			}
+			SetFileds(xdoc);
 		}
 	}
 }
